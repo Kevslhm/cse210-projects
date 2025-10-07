873e39a5ea575718242810a4a7fe30782a46bc42
Kevslhm <kevsherma@gmail.com>
2025-10-07 13:22:46 -0600

    Prep 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userPercentage = Console.ReadLine();
+        int numPercentage = int.Parse(userPercentage);
+
+        if (numPercentage >= 90)
+        {
+            Console.WriteLine($"you've got an A with grade of {numPercentage}");
+        }
+        else if (numPercentage >= 80 && numPercentage < 90)
+        {
+            Console.WriteLine($"you've got a b with a grade of {numPercentage}");
+        }
+        else if (numPercentage >= 70 && numPercentage < 80)
+        {
+            Console.WriteLine($"you've got a c with a grade of {numPercentage}");
+        }
+        else if (numPercentage >= 60 && numPercentage < 70)
+        {
+            Console.WriteLine($"you've got a d with a grade of {numPercentage}");
+        }
+        else
+        {
+            Console.WriteLine($"you've got a f with a grade of {numPercentage}");
+        }
     }
 }
