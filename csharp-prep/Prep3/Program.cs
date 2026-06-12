6bd4fd76e987aa9331e06f440df7e7943f333a75
Kevslhm <kevsherma@gmail.com>
2026-06-11 21:23:20 -0600

    changed de code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("what is the magic number? ");
+        string magicNum = Console.ReadLine();
+        int number = int.Parse(magicNum);
+        int guess = 0;
+
+        while (number != guess)
+        {
+
+            Console.WriteLine("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == number)
+            {
+                Console.WriteLine("That is the number");
+            }
+        }
+
+
     }
 }
