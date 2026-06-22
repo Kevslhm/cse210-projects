ed434f92738de14ed1a12e4340d5b1991877dc93
Kevslhm <kevsherma@gmail.com>
2026-06-22 16:26:20 -0600

    finished the strecht challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
@@ -7,11 +8,15 @@ class Program
         string play_again = "yes";
         Random randomGenerator = new Random();
         int number = randomGenerator.Next(1,100);
-        int guess = 0;
+        int guess;
+        int attemps = 0;
 
         while (play_again != "no")
         {
 
+            Console.WriteLine();
+            Console.WriteLine("Attemps: " + attemps);
+            Console.WriteLine();
             Console.Write("What is your guess? ");
             string userGuess = Console.ReadLine();
             guess = int.Parse(userGuess);
@@ -19,14 +24,17 @@ class Program
             if (guess > number)
             {
                 Console.WriteLine("Lower");
+                attemps++;
             }
             else if (guess < number)
             {
                 Console.WriteLine("Higher");
+                attemps++;
             }
             else if (guess == number)
             {
                 Console.WriteLine("That is the number!!");
+                attemps = 0;
                 Console.Write("Do you wanna play again? (yes/no)  ");
                 play_again = Console.ReadLine();
                 number = randomGenerator.Next(1,100);
